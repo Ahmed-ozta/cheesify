a73d83e4688f2dd73510357a1caa8cf5b908d2e5
Ahmed-ozta <ahmedchanane90@gmail.com>
2025-01-07 12:46:46 +0100

    nav bar coding

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -3,43 +3,62 @@ function Nav() {
 
     function displayMenu(): void {
         const menu = document.getElementById("menu");
-        if (menu) {
-            menu.style.display = `${!menu.style.display}`;
+        const barMenu = document.getElementById("barMenu")
+        if (menu && barMenu) {
+            if (menu.style.display === "none" || !menu.style.display) {
+                menu.style.display = "block"
+                setTimeout(() => {
+                    barMenu.style.opacity = "1";
+                    barMenu.classList.add("slideDown");
+                }, 1000)
+            } else {
+                menu.style.display = "none";
+                barMenu.style.opacity = "0";
+                barMenu.classList.remove("slideDown")
+            }
         }
 
     }
     return (
 
         <div className={style.container}>
-            <nav>
+            <div className={style.subContainer}>
+                <nav>
 
-                <div className={style.main}>
-                    <div className={style.left}>
-                        <img src="public\Group 6.svg" />
-                        <h1>Chessify</h1>
-                    </div>
-                    <div className={style.right}>
-                        <ul>
-                            <li><a>Home</a></li>
-                            <li><a>Play</a></li>
-                            <li><a>Game rules</a></li>
-                            <li><a>Contact</a></li>
-                            <li><a>Champions</a></li>
-                        </ul>
-                        <div className={style.small}>
-                            <button onClick={() => displayMenu()} >Menu</button>
-                            <ul id="menu">
-                                <li><a>Home</a></li>
-                                <li><a>Play</a></li>
-                                <li><a>Game rules</a></li>
-                                <li><a>Contact</a></li>
-                                <li><a>Champions</a></li>
-                            </ul>
+                    <div className={style.main}>
+                        <div className={style.left}>
+                            <img src="public\Group 6.svg" />
+                            <h1>Chessify</h1>
+                        </div>
+                        <div className={style.right}>
+                            <div className={style.big}>
+                                <ul>
+                                    <li><a>Home</a></li>
+                                    <li><a>Play</a></li>
+                                    <li><a>Game rules</a></li>
+                                    <li><a>Contact</a></li>
+                                    <li><a>Champions</a></li>
+                                </ul>
+                            </div>
+                            <div className={style.small}>
+                                <button onClick={() => displayMenu()} ><img src="public\Hamburger_MD.svg" /></button>
+
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </nav>
+                </nav>
+                <div id="barMenu" className={style.small}>
+                    <ul id="menu" >
+                        <li><a>Home</a></li>
+                        <li><a>Play</a></li>
+                        <li><a>Game rules</a></li>
+                        <li><a>Contact</a></li>
+                        <li><a>Champions</a></li>
+                    </ul>
+
+                </div>
+            </div>
         </div>
     )
 }
